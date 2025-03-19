822383484535c2415d1fdd7bedb57f2dffa50b83
justjo3l <comedyandcomputers@gmail.com>
2025-03-19 17:14:56 +1100

    Add CORS headers to responses

--- lambda_functions/retrievalJSON/lambda_function.py
+++ lambda_functions/retrievalJSON/lambda_function.py
@@ -20,7 +20,11 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "headers": {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+                "Access-Control-Allow-Headers":
+                "Content-Type, Authorization"
             },
             "body": parsed_json
         }
